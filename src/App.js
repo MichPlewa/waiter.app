48ea07218141dded20f4c3153048d4811e8a190b
Michal Plewa <themipsial@gmail.com>
2022-06-30 21:33:08 +0200

    prepate routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import { Container } from 'react-bootstrap';
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/pages/Home';
+import Table from './components/pages/Table';
 
 const App = () => {
-  return <Container>Hallo World</Container>;
+  return (
+    <Container>
+      <div>Hallo World</div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/table/:id" element={<Table />} />
+      </Routes>
+    </Container>
+  );
 };
 
 export default App;
